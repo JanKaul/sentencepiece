e84e63fb4370efaa586c590e93266ed0d54f1862
Jan Kaul <jankaul@mailbox.org>
2021-10-18 10:58:07 +0200

    optimize build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ mkdir build
 cd build
 emcmake cmake ../sentencepiece
 emmake make sentencepiece-static
-emcc --bind -o ../src/sentencepiece.js -Wl,--whole-archive src/libsentencepiece.a -Wl,--no-whole-archive ../bindings/sentencepiece.cpp -s EXPORT_ES6=1 -s MODULARIZE=1 -s SINGLE_FILE=1 -s ASYNCIFY=1
+emcc --bind -o ../src/sentencepiece.js -Wl,--whole-archive src/libsentencepiece.a -Wl,--no-whole-archive ../bindings/sentencepiece.cpp -s EXPORT_ES6=1 -s MODULARIZE=1 -s SINGLE_FILE=1 -O3 -s ASYNCIFY=1
 cd ..
